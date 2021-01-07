9f552bb78a885bea8388d327cd3e008dd2a364c4
Dariwoos <tst.info20@gmail.com>
2021-01-07 16:20:51 +0000

    instagram db

--- src/models.py
+++ src/models.py
@@ -8,24 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Post(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    description= Column(String(250) , nullable=True)
+    image_url= Column(String(250))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    useer = relationship (User)
+    comment_id = Column(Integer, ForeignKey('comments.id'))
+    comments = relationship("Comment", backref="post")
+   
 
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer,primary_key=True)
+    length= Column(String(250))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship(Post)
+
+
+
+class Follwoers(Base):
+    __tablename__ = 'Followers'
+    id= Column(Integer, primary_key=True)
+    Followed = Column(Integer, ForeignKey('users.id'))
+    Following = Column(Integer, ForeignKey('users.id'))
     def to_dict(self):
         return {}
 
